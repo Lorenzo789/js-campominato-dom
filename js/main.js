96f2ca24d289cfa2bf1d8aff452aebd4a6f1d855
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-24 12:04:44 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+
+const btnGameStart = document.getElementById('btn-grid-game')
+
+
+btnGameStart.addEventListener( 'click', function() {
+
+    document.getElementById("btn-grid-game").disabled = true;
+    
+    const parentGrid = document.getElementById("grid-game");
+    
+    parentGrid.classList.add('grid-container')
+    
+    for (let index = 0; index < 100; index++) {
+        
+        const newSquare = createNewSquare();
+        
+        newSquare.innerHTML = (index + 1);
+        
+        parentGrid.append(newSquare);
+        
+        newSquare.addEventListener( 'click', function(){
+    
+            newSquare.classList.toggle('active');
+
+            console.log('hai cliccato il numero' , + index + 1);
+    
+        })
+
+    }
+})
+
+
+// * FUNCTIONS * \\
+function createNewSquare() {
+    const square = document.createElement('div');
+
+    square.classList.add('square');
+
+    return square;
+}
+
+
+
+
+
+/*  generare 16 bombe casuali
+ nell' array delle bombe non possono esserci due numeri uguali.
+
+ click sulla cella blu se va bene rossa se è una bomba e fine partita (disabilitare il click)
+
+ punteggio a fine partita con un alert? o semplicemente in console
+
+
+*/
