dc01b7783cc2a81e418f8379b07ee33eb114f969
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-24 18:40:41 +0200

    done

--- js/main.js
+++ js/main.js
@@ -11,71 +11,96 @@
 */
 
 // array bombe
-const listBomb = [];
+let listBomb = [];
  
+// variabile bomba presa
 let endGame;
 
+// prendo il bottone per iniziare il gioco
 const btnGameStart = document.getElementById('btn-grid-game')
 
 btnGameStart.addEventListener( 'click', function() {
 
     endGame = false;
 
+    // disabilito il click del pulsante
     document.getElementById("btn-grid-game").disabled = true;
     
+    // creo una variabile per inserire poi i miei quadratini
     let parentGrid = document.getElementById("grid-game");
     
+    // aggiungo una classe al parent
     parentGrid.classList.add('grid-container');
     
-    for (let index = 0; index < 17; index++) {
-    
-        const bombUnique = bombGenerator (listBomb, 1 , 100);
-    
-        listBomb.push(bombUnique);
+    listBomb = [];
+
+    // ciclo for per la generazione casuale di 16 bombe
+    for (let index = 0; index <= 16; index++) {
         
-        console.log( index + 1 , listBomb);
+        // variabile per la generazione delle bombe
+        const bombUnique = bombGenerator (listBomb, 1 , 100);
+        
+        // inserire le bombe generate casualmente nell'array
+        listBomb.push(bombUnique);
+
+        console.log(listBomb);
     }
-    
+
+    // variabile per la condizione di vittora
+    let winGame = ((100) - (16));
+
+    // ciclo for per la creazione dei quadratini
     for (let index = 0; index < 100; index++) {
         
+        // funzione per la creazione di 100 div
         const newSquare = createNewSquare();
         
-        let winGame = ((index + 1) - 16);
-        console.log(winGame);
-
-        newSquare.innerHTML = (index + 1);
-        
+        // inserisco i 100 div nel parent
         parentGrid.append(newSquare);
 
         newSquare.addEventListener( 'click', function(){
 
+            // condizione array bombe
             if (listBomb.includes(index + 1)) {
 
+                // aggiungere classe active-bomb
                 newSquare.classList.add('active-bomb');  
 
-                // endGame = true;
+                // variabile sconfitta
+                endGame = true;
 
+                // se la variabile è true
                 if (endGame == true) {
                     
+                    // svuota il parent da tutti i suoi div
                     parentGrid.innerHTML = null;
 
                     parentGrid.classList.remove('grid-container');
 
                     alert('Hai Perso!!!! Premi Play per Riprovarci');
 
+                    // riabilita il click del bottone play
                     document.getElementById("btn-grid-game").disabled = false;
                 }
 
             } else {
                 
                 newSquare.classList.add('active');
+                
+                // variabile vittoria
+                let counter = document.querySelectorAll(".active").length;
 
-                if (newSquare.className('active') == winGame){
+                // condizione vittoria
+                if ( counter == winGame) {
                     
-                    console.log('hai vinto');
+                    alert('Hai Vinto Premi Play per giocare ancora')
+                    
+                    parentGrid.innerHTML = null;
+    
+                    document.getElementById("btn-grid-game").disabled = false;
                 }
             }
-            
+
         }) 
     }
 })
@@ -110,3 +135,4 @@ function bombGenerator (bombNumberList, min , max) {
 
     return randomBomb;
 }
+// * FUNCTIONS * \\
